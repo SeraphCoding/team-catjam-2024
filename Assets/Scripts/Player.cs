71c3b17ec7693cf48ef7a0ad2a53f018b850a2a3
Dragon Warior <evelyn.gravett@protonmail.com>
2024-07-24 17:55:47 +0100

    feat: make player into prefab, update spritesheet, movement animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,13 @@ public class Player : MonoBehaviour
     private PlayerControls _playerControls;
     private Vector2 _movement;
     private Rigidbody2D _body;
+    private Animator _anim;
 
     private void Awake()
     {
         _playerControls = new PlayerControls();
         _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -34,10 +36,29 @@ public class Player : MonoBehaviour
     private void PlayerInput()
     {
         _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+
+        if (_movement.magnitude == 0) {
+            if (_anim.speed != 0) StopAnimation();
+        } else {
+            if (_anim.speed == 0) StartAnimation();
+            _anim.SetFloat("X", _movement.x);
+            _anim.SetFloat("Y", _movement.y);
+        }
     }
 
     private void Move()
     {
         _body.MovePosition(_body.position + _movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void StopAnimation()
+    {
+        _anim.speed = 0;
+        _anim.Play(0, 0, 0); // set animation to first frame
+    }
+
+    private void StartAnimation()
+    {
+        _anim.speed = 1;
     }
 }
