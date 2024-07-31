46ee8319ac52501924550a68429277925cd5eb99
SeraphCode <yannikland@gmail.com>
2024-07-31 07:26:51 +0200

    Fix dialog not opening during testing in editor when save system is not present

--- Assets/Scripts/SceneManagers/LevelSceneManager.cs
+++ Assets/Scripts/SceneManagers/LevelSceneManager.cs
@@ -10,7 +10,10 @@ namespace SceneManagers
         // Start is called before the first frame update
         void Start()
         {
-            SaveSystemSingleton.Instance.UpdateCurrentScene(sceneName);
+            if (SaveSystemSingleton.Instance)
+            {
+                SaveSystemSingleton.Instance.UpdateCurrentScene(sceneName);
+            }
             if (dialog) hud.ShowDialog();
         }
     }
